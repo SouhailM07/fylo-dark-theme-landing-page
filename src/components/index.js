cd32d799d222a3af86c6b5e9f51d6036d073fd6d
shadow <lightshadow416@gmail.com>
2023-11-16 15:28:14 +0100

    [MyContainer / Navbar / Hero / Footer] components were created

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as MyContainer } from "./MyContainer/MyContainer";
+export { default as Footer } from "./Footer/Footer";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Hero } from "./Hero/Hero";
