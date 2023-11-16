667547cfe72d32d8aa17f667be8389ae2651a51b
shadow <lightshadow416@gmail.com>
2023-11-16 15:56:39 +0100

    [Navbar] component -> gpt

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./style/input.css";
-import "./App.css";
+// import "./App.css";components
+import MyContainer from "./components/MyContainer/MyContainer";
+
 function App() {
+  let body = document.body;
+  body.className = "bg-dark-blue-main";
   return (
     <>
-      <div>sb</div>
+      <MyContainer />
     </>
   );
 }
