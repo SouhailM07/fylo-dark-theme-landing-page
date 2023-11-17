8ebb74f912031be39ba78977641f317b740df15a
shadow <lightshadow416@gmail.com>
2023-11-17 18:30:57 +0100

    [Navbar] component is now /RESPONSIVE/

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ import logoImg from "../../../public/logo.svg";
 export default function Logo() {
   return (
     <>
-      <img src={logoImg} alt="Logo" className="h-auto w-auto" />
+      <img src={logoImg} alt="Logo" className="md:h-auto md:w-auto h-[2rem]" />
     </>
   );
 }
