7f6d514fb80f670dc6b849aa1db1f0d30e0f2605
shadow <lightshadow416@gmail.com>
2024-01-29 16:14:47 +0100

    adding [/Helmet/]

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import "./style/input.css";
 import "./App.css";
 //components
 import MyContainer from "./components/MyContainer/MyContainer";
+import { Helmet } from "react-helmet";
 
 function App() {
   let body = document.body;
   body.className = "bg-[#181f2b]";
   return (
     <>
+      <Helmet>
+        <meta name="description" content="this is front end mentor challenge" />
+        <meta
+          name="keywords"
+          content="landing page ,responsive design,frontendmentor"
+        />
+        <meta name="author" content="Shadow" />
+      </Helmet>
       <MyContainer />
     </>
   );
